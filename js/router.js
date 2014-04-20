bea5e86f6368c2e65f29d503e15ff9f5f7568002
Sergey S <5818959@gmail.com>
2014-04-20 12:46:43 +0400

    [c] Transitioning to show only incomplete todos.

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
-  	// body...
+  	// additional child routes
+  	this.route('active');
   });
 });
 
@@ -8,7 +9,18 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 	model: function() {
 		return this.modelFor('todos');
 	}
-})
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
+	}
+});
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
